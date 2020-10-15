e1d6a60f7843024fd91d8daa1b5ffbea29fe7d8c
sin <379457721@qq.com>
2020-10-15 10:16:38 +0800

    fix annotation error

--- android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
+++ android/src/main/java/com/airship/customwebview/VideoWebChromeClient.java
@@ -3,7 +3,7 @@ package com.airship.customwebview;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
